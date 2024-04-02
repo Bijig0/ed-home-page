9f9253b5ad9df796eaa6d279a304672fa7c5187b
Brady <bradysuryasie@gmail.com>
2024-04-02 14:21:55 +1100

    nice added emailjs

---
+++ src/components/MainForm/FormCompleted.tsx
@@ -0,0 +1,90 @@
+import emailjs from "@emailjs/browser";
+import { useMutation } from "@tanstack/react-query";
+import { useEffect } from "react";
+import useFormStore from "./useFormStore";
+
+type ErrorTextProps = {
+  children: React.ReactNode;
+};
+
+const ErrorText = (props: ErrorTextProps) => {
+  return (
+    <p className="text-danger text-3" style={{ fontSize: "12px" }}>
+      {props.children}
+    </p>
+  );
+};
+
+const FormCompleted = () => {
+  const studentDetails = useFormStore((state) => state.studentDetails);
+
+  const sendEmail = async () => {
+    const templateParams = {
+      to_name: "Brady",
+      from_name: "Tutoring",
+      subject: "New Tutoring Person",
+      message: `New tutoring person, details: ${JSON.stringify(studentDetails)}`,
+    };
+
+    const serviceId = "service_010xydf";
+    const templateName = "template_1dcm4rn";
+    const publicKey = "Yd6r5t5etWEKD3GNh";
+    return emailjs.send(serviceId, templateName, templateParams, publicKey);
+  };
+
+  const {
+    mutate: sendEmailMutate,
+    isPending,
+    error,
+    isSuccess,
+  } = useMutation({
+    mutationFn: sendEmail,
+    mutationKey: ["sendEmail"],
+  });
+
+  useEffect(() => {
+    sendEmailMutate();
+  }, []);
+
+  if (isPending) {
+    return null;
+  }
+
+  if (isSuccess) {
+    return (
+      <>
+        <SuccessToast />
+        <p>Success good job, all completed</p>
+      </>
+    );
+  }
+
+  if (error) {
+    return (
+      <>
+        <ErrorToast />
+        <p>Error, something went wrong</p>
+      </>
+    );
+  }
+
+  return null;
+};
+
+const SuccessToast = () => {
+  return (
+    <div className="alert alert-success" role="alert">
+      We have received your quote request and will be in touch soon!
+    </div>
+  );
+};
+
+const ErrorToast = () => {
+  return (
+    <div className="alert alert-danger" role="alert">
+      Something went wrong, please try again
+    </div>
+  );
+};
+
+export default FormCompleted;
--- src/components/MainForm/MainForm.tsx
+++ src/components/MainForm/MainForm.tsx
@@ -1,4 +1,6 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Wizard } from "react-use-wizard";
+import FormCompleted from "./FormCompleted";
 import Step1 from "./Step1";
 import Step2 from "./Step2";
 import Step3 from "./Step3";
@@ -8,19 +10,24 @@ import Step6 from "./Step6";
 import Step7 from "./Step7";
 import Step8 from "./Step8";
 
+const queryClient = new QueryClient();
+
 const MainForm = () => {
   return (
     <div className="main-form">
-      <Wizard>
-        <Step1 />
-        <Step2 />
-        <Step3 />
-        <Step4 />
-        <Step5 />
-        <Step6 />
-        <Step7 />
-        <Step8 />
-      </Wizard>
+      <QueryClientProvider client={queryClient}>
+        <Wizard>
+          <Step1 />
+          <Step2 />
+          <Step3 />
+          <Step4 />
+          <Step5 />
+          <Step6 />
+          <Step7 />
+          <Step8 />
+          <FormCompleted />
+        </Wizard>
+      </QueryClientProvider>
     </div>
   );
 };
