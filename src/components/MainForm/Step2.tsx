81ac1aa1b0ced969fef568bd23491cad67ee681c
Brady <bradysuryasie@gmail.com>
2024-04-03 15:12:28 +1100

    trying to add more styles

--- src/components/MainForm/Step2.tsx
+++ src/components/MainForm/Step2.tsx
@@ -83,11 +83,11 @@ const Step1 = () => {
         </div>
       </div>
       <div className="flex flex-[2_2_0%] flex-col justify-end text-center p-8 items-center font-semibold">
-        <div className="bg-light rounded-lg flex justify-center flex-col items-center px-12 py-8 gap-4">
-          <IoDocumentTextSharp className="icon" size={72} color="#f43f5e" />
-          <p className="text-black text-lg font-primary font-light">
+        <div className="bg-light rounded-lg flex justify-center flex-col items-center px-8 py-8 gap-4">
+          <IoDocumentTextSharp className="icon" size={60} color="#f43f5e" />
+          <p className="text-black text-md font-primary font-light">
             Our experienced tutors{" "}
-            <span className="inline text-black text-lg font-primary font-semibold">
+            <span className="inline text-black text-md font-primary font-semibold">
               individualize their learning plans
             </span>{" "}
             to match each student's personal needs and goals!
--- src/components/MainForm/Step3.tsx
+++ src/components/MainForm/Step3.tsx
@@ -81,10 +81,10 @@ const Step1 = () => {
           </button>
         </div>
       </div>
-      <div className="flex-[2_2_0%] relative self-end h-64 bg-light rounded-lg flex justify-center flex-col px-12 gap-4">
+      <div className="flex-[2_2_0%] relative self-end h-60 bg-light rounded-lg flex justify-center flex-col px-12 gap-4">
         <div className="flex flex-col justify-end items-start gap-5">
           <FaQuoteLeft className="icon" size={16} />
-          <p className="text-black text-lg font-primary font-light mt-2">
+          <p className="text-grey text-sm font-primary font-light mt-2">
             Varsity Tutors really helped me understand the concepts during my
             Calculus 2 class. My tutor has such amazing patience and is open to
             many questions! Lastly, was always on time and very prompt. Highly
@@ -95,6 +95,9 @@ const Step1 = () => {
             color="white"
             size={80}
           />
+          <p className="text-grey text-sm font-primary font-light">
+            -- John Woo
+          </p>
         </div>
       </div>
     </div>
