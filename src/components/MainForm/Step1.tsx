2bb445a00a2bff4bf58c560338ecf47c0de2a94c
Brady <bradysuryasie@gmail.com>
2024-04-03 13:59:41 +1100

    trying to add more styles

--- src/components/MainForm/Step1.tsx
+++ src/components/MainForm/Step1.tsx
@@ -37,8 +37,10 @@ const Step1 = () => {
           Welcome! Let's match you with the perfect programming tutor
         </p>
 
-        <button className="flex text-center mt-6 md:mx-auto md:justify-center md:w-60 md:items-center px-6 py-2 font-semibold text-white bg-rose-500 rounded-lg">
-          <p className="text-light text-lg text-center">Who needs tutoring?</p>
+        <button className="flex text-center mt-6 mx-auto justify-center w-64 items-center px-6 py-2 font-semibold bg-rose-500 rounded-lg">
+          <p className="text-light text-lg text-center m-0">
+            Who needs tutoring?
+          </p>
         </button>
 
         <form className="flex flex-col items-center justify-center">
