a78e84d0f27403c8c35e8d8654c4ae619eca1c67
Brady <bradysuryasie@gmail.com>
2024-04-11 14:39:21 +1000

    whyUs better

---
+++ src/lib/whyUs.ts
@@ -0,0 +1,46 @@
+type WhyUs = {
+    title: string;
+    description: string;
+    image: string;
+}
+
+const whyUs = [
+  {
+    title: "Safety First",
+    description:
+      "We are a team of experienced teachers who are dedicated to providing a safe and supportive learning environment for all students. We understand that safety is a top priority and we take every precaution to ensure that our students are protected at all times.",
+    image: "https://img.icons8.com/bubbles/50/shield.png",
+  },
+  {
+    title: "Teaching With A Passion",
+    description:
+      "Our teachers are passionate about teaching and are committed to providing high-quality education to our students. We believe in the power of education and we are dedicated to helping our students achieve their full potential.",
+    image: "https://img.icons8.com/bubbles/50/training.png",
+  },
+  {
+    title: "Stay In The Loop",
+    description:
+      "We are always looking for ways to improve our teaching methods and provide our students with the best possible learning experience. We believe in continuous improvement and we are always looking for new ways to enhance our teaching approach.",
+    image: "https://img.icons8.com/bubbles/50/link.png",
+  },
+  {
+    title: "Don't fret about payments",
+    description:
+      "We understand that payments can be a source of stress for many students. That's why we offer flexible payment options to make the process as smooth as possible. We also provide a detailed payment plan to help you understand the costs associated with our services.",
+    image: "https://img.icons8.com/bubbles/50/receive-cash.png",
+  },
+  {
+    title: "Direct Communication",
+    description:
+      "We believe in open communication and transparency. We encourage our students to ask questions and seek clarification whenever needed. We also provide a dedicated support team to help you with any issues you may encounter.",
+    image: "https://img.icons8.com/bubbles/50/chat.png",
+  },
+  {
+    title: "First Lesson On Us",
+    description:
+      "We understand that every student is unique and has different learning needs. That's why we offer a personalized learning experience tailored to your specific needs. We also provide a variety of resources and materials to help you succeed in your studies.",
+    image: "https://img.icons8.com/bubbles/50/filled-like.png",
+  },
+] satisfies WhyUs[];
+
+export default whyUs;
