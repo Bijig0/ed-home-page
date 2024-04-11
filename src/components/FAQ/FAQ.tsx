c299fd78673c4874519f637f41f0fd6d3b0c5deb
Brady <bradysuryasie@gmail.com>
2024-04-12 00:32:35 +1000

    trying to add more fAQ

--- src/components/FAQ/FAQ.tsx
+++ src/components/FAQ/FAQ.tsx
@@ -30,91 +30,35 @@ const SingleAccordion = (props: SingleAccordionProps) => {
   const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
 
   return (
-    <div key={title} className="accordion-item">
-      <div className={`accordion-header`}>
-        <button
-          {...getToggleProps()}
-          className={`accordion-button ${isExpanded ? undefined : "collapsed"}`}
-        >
-          {title}
-        </button>
-      </div>
-      <div
-        {...getCollapseProps()}
-        className="accordion-collapse collapse show"
-        id="ac_1"
-        data-bs-parent="#accordion_1"
-      >
-        <div className="accordion-body">
-          <p>{text}</p>
+    <div key={title} className="el-accordion">
+      <div className="el-accordion-head">
+        <div className="inner d-flex align-items-center">
+          <div className="accordion-image">
+            <img src="assets/images/icons/click.svg" alt="Click me" />
+          </div>
+          <div {...getToggleProps()} className="accordion-title">
+            <h4>{title}</h4>
+          </div>
+          <div className="accordion-arrow">
+            <div className="display-center">
+              <span className="fa fa-angle-down"></span>
+            </div>
+          </div>
         </div>
+      </div>
+      <div {...getCollapseProps()} className="el-accordion-body">
+        <p>{text}</p>
       </div>
     </div>
   );
 };
-
 const FAQ = () => {
-  const faqFirstHalf = faqList.slice(0, 3);
-  const faqSecondHalf = faqList.slice(3);
   return (
-    <div className="faq-section pt-120 pb-120">
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-xl-7 col-lg-8">
-            <div className="at-section-title text-center">
-              <span className="at-subtitle position-relative text-primary lead">
-                FAQs
-              </span>
-              <h2 className="h1 mb-0 mt-2">Frequently Asked Questions</h2>
-            </div>
-          </div>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-xl-10">
-            <div className="faq-tabs mt-5 brands-filter">
-              <div className="tab-content mt-60">
-                <div className="tab-pane fade show active" id="general">
-                  <div className="row g-4">
-                    <div className="col-xl-6">
-                      <div
-                        className="accordion theme-accordion"
-                        id="accordion_1"
-                      >
-                        {faqFirstHalf.map(({ title, text }) => {
-                          console.log({ title, text });
-                          return (
-                            <SingleAccordion
-                              isExpanded={true}
-                              title={title}
-                              text={text}
-                            />
-                          );
-                        })}
-                      </div>
-                    </div>
-                    <div className="col-xl-6">
-                      <div
-                        className="accordion theme-accordion"
-                        id="accordion_1"
-                      >
-                        {faqSecondHalf.map(({ title, text }) => {
-                          return (
-                            <SingleAccordion
-                              isExpanded={true}
-                              title={title}
-                              text={text}
-                            />
-                          );
-                        })}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className="items mb--15">
+      {faqList.map(({ title, text }) => {
+        console.log({ title, text });
+        return <SingleAccordion isExpanded={true} title={title} text={text} />;
+      })}
     </div>
   );
 };
