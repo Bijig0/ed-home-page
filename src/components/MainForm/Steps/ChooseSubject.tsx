e3feb83d1c81a77aa4803230fae482f9ad2b5cf1
Brady <bradysuryasie@gmail.com>
2024-05-01 08:59:26 +1000

    fixed up styles for year 11 choose subjects

--- src/components/MainForm/Steps/ChooseSubject.tsx
+++ src/components/MainForm/Steps/ChooseSubject.tsx
@@ -1,12 +1,11 @@
-import { FaCaretDown, FaQuoteLeft } from "react-icons/fa";
 import { useWizard } from "react-use-wizard";
-import { reviewThree } from "../../../reviews";
 import FormPartLayout from "../FormPartLayout";
 import LeftSideFormPartLayout from "../LeftSideFormPartLayout";
 import SelectButton from "../SelectButton";
 import yearByYearDetails, { type Subject } from "../types/subjects";
 import { type WhoNeedsTutoring } from "../types/types";
 import { studentDetails, updateStudentDetails } from "../useFormStore";
+import { useMediaQuery } from "react-responsive";
 
 const headerText = {
   Child: "What subject does your child need help with?",
@@ -31,8 +30,52 @@ const ChooseSubjects = () => {
 
   console.log(studentDetails.get());
 
+  const firstHalfSubjects = subjects.slice(0, Math.floor(subjects.length / 2));
+  const secondHalfSubjects = subjects.slice(Math.floor(subjects.length / 2));
+
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 1224px)",
+  });
+
+  console.log({ isDesktopOrLaptop });
+
+  if (isDesktopOrLaptop) {
+    return (
+      <FormPartLayout>
+        <LeftSideFormPartLayout
+          activeStep={activeStep}
+          headerText={headerText[studentDetails.get().whoNeedsTutoring]}
+        >
+          {firstHalfSubjects.map((subject) => (
+            <SelectButton
+              key={subject}
+              selected={(subject) => subject === studentDetails.get().subject}
+              value={subject}
+              text={subject}
+              handleSubmit={handleSubmit}
+            />
+          ))}
+        </LeftSideFormPartLayout>
+
+        <div className="flex flex-1">
+          <div className="flex flex-col justify-end ">
+            {secondHalfSubjects.map((subject) => (
+              <SelectButton
+                key={subject}
+                selected={(subject) => subject === studentDetails.get().subject}
+                value={subject}
+                text={subject}
+                handleSubmit={handleSubmit}
+              />
+            ))}
+          </div>
+        </div>
+      </FormPartLayout>
+    );
+  }
+
   return (
-    <FormPartLayout >
+    <FormPartLayout>
       <LeftSideFormPartLayout
         activeStep={activeStep}
         headerText={headerText[studentDetails.get().whoNeedsTutoring]}
@@ -46,23 +89,6 @@ const ChooseSubjects = () => {
           />
         ))}
       </LeftSideFormPartLayout>
-
-      <div className="hidden-mobile-flex-normal flex-[2_2_0%] relative self-end h-72 bg-light rounded-lg justify-center flex-col px-12 mb-4">
-        <div className="flex flex-col justify-end items-start gap-5">
-          <FaQuoteLeft className="icon" size={16} color="#f43f5e" />
-          <p className="text-black text-base font-primary font-light mt-2">
-            {reviewThree.text}
-          </p>
-          <FaCaretDown
-            className="icon absolute left-8 bottom-[-38px]"
-            color="white"
-            size={80}
-          />
-          <p className="text-black text-base font-primary font-light">
-            -- {reviewThree.author}
-          </p>
-        </div>
-      </div>
     </FormPartLayout>
   );
 };
