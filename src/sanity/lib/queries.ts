a0c9ac2e1a0d1f3d365d250e51fffaab2622bc4e
Brady <bradysuryasie@gmail.com>
2024-05-22 11:25:17 +1000

    testing

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -1,13 +1,11 @@
-import { groq } from "next-sanity";
-
-export const POSTS_QUERY = groq`*[_type == "post" && defined(slug)] {
+export const POSTS_QUERY = `*[_type == "post" && defined(slug)] {
  ...,
  "author": author->,
  "excerpt": array::join(string::split((pt::text(body)), "")[0..255], "") + "...",
  "categories": categories[]->{title}
 }`;
 
-export const POST_QUERY = groq`*[_type == "post" && slug.current == $slug][0] {
+export const POST_QUERY = `*[_type == "post" && slug.current == $slug][0] {
  ...,
  "author": author->,
  "excerpt": array::join(string::split((pt::text(body)), "")[0..255], "") + "...",
